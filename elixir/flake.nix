b4dd52c325e4e973ff094655411f14876751ccb7
julienurraca@gmail.com <julienurraca@gmail.com>
2021-08-30 14:38:28 -0500

    refactor w/ overlay

--- elixir/flake.nix
+++ elixir/flake.nix
@@ -6,30 +6,34 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    # specify the system since Nix doesn't have access to the currentSystem value.
-    with import nixpkgs { system = "x86_64-linux";};
+  outputs = { self, nixpkgs, utils }: 
     let
-      pkgsForSystem = system: rec {
-        inherit system beamPackages;
-        deps = with pkgs; import ./deps.nix { inherit beamPackages lib ;};
-        pkgs = beam.packagesWith beam.interpreters.erlang;
-        my-mix-project = pkgs.mixRelease rec {
+    pkgsForSystem = system: import nixpkgs {
+        overlays = [ overlay ];
+        inherit system;
+      };
+
+    overlay = final: prev: rec {
+
+      my-mix-project = with final;
+        let 
+          beamPackages = beam.packagesWith beam.interpreters.erlangR24; 
+          mixNixDeps = import ./deps.nix { inherit lib beamPackages; }; 
+        in beamPackages.mixRelease {
           pname = "my-mix-project";
           src = ./.;
           version = "0.0.0";
-          mixNixDeps = deps;
-        };
-      };
-    in utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ] (system: rec {
+         };
+      devShell = final.my-mix-project;
+    };
+    in utils.lib.eachDefaultSystem (system: rec {
       legacyPackages = pkgsForSystem system;
       packages = utils.lib.flattenTree {
-        inherit (legacyPackages) my-mix-project;
+        inherit (legacyPackages) devShell my-mix-project;
       };
       defaultPackage = packages.my-mix-project;
-      devShell = pkgs.mkShell { buildInputs = [ packages.my-mix-project ] ;};
       apps.my-mix-project = utils.lib.mkApp { drv = packages.my-mix-project; };
       hydraJobs = { inherit (legacyPackages) my-mix-project; };
       checks = { inherit (legacyPackages) my-mix-project; };
-  });
+    });
 }
